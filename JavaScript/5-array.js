1594ffa4a70fe44bed9af3c6d08a3620228873c8
tshemsedinov <timur.shemsedinov@gmail.com>
2018-09-17 18:44:56 +0300

    Add more examples

---
+++ JavaScript/5-array.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const welcome = person => {
+  console.log(`Ave, ${person.name}!`);
+};
+
+const persons = [
+  { name: 'Marcus Aurelius' },
+  { name: 'Mao Zedong' },
+  { name: 'Rene Descartes' },
+];
+
+for (const person of persons) {
+  welcome(person);
+}
---
+++ JavaScript/6-hash.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const welcome = person => {
+  console.log(`Ave, ${person.name}!`);
+};
+
+const persons = {
+  marcus: { name: 'Marcus Aurelius' },
+  mao: { name: 'Mao Zedong' },
+  rene: { name: 'Rene Descartes' },
+};
+
+for (const name in persons) {
+  const person = persons[name];
+  welcome(person);
+}
