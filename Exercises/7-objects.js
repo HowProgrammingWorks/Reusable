af18338fbec103fa7920398837f498b32ce16cad
Koroliuk <54640416+Koroliuk@users.noreply.github.com>
2019-09-17 17:54:31 +0300

    Update 7-objects.js

--- Exercises/7-objects.js
+++ Exercises/7-objects.js
@@ -1,5 +1,17 @@
 'use strict';
 
-const fn = null;
+const fn = () => {
+  const obj1 = { name };
+  let obj2 = { name };
+  obj1.name = 'Ivan';
+  obj2.name = 'Sasha';
+  /*obj1 = { name: 'Yurii' };
+  так як obj1 об'явлено як const,
+  то ми можемо змінювати поля obj1,
+  але не можемо константному об'єкту
+  присвоїти інше значення
+  */
+  obj2 = { name: 'Petro' };
+};
 
 module.exports = { fn };
