28257f6ef5ac75aecb22ba2cf7be31e1e8da0cc6
Developer <developer@localhost.localdomain>
2019-09-18 17:10:53 +0300

    Tasks Reusable

--- Exercises/5-range-odd.js
+++ Exercises/5-range-odd.js
@@ -1,5 +1,11 @@
 'use strict';
 
-const rangeOdd = null;
+const rangeOdd = (start, end) => {
+  const array = [];
+  for (let number = start; number <= end; number++) {
+    if (number % 2) array.push(number);
+  }
+  return array;
+};
 
 module.exports = { rangeOdd };
