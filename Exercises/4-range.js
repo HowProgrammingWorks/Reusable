335c530a0b2f2b3e71b87ff1f1ca44fb01a6432f
Kathrin <kathrin.shpigel.90@gmail.com>
2019-09-18 19:35:26 +0300

    Labs Reusable completed

--- Exercises/4-range.js
+++ Exercises/4-range.js
@@ -1,5 +1,11 @@
 'use strict';
 
-const range = null;
+const range = (start, end) => {
+  const res = [];
+  for (let i = start; i <= end; i++) {
+    res.push(i);
+  }
+  return res;
+};
 
 module.exports = { range };
--- Exercises/5-range-odd.js
+++ Exercises/5-range-odd.js
@@ -1,5 +1,10 @@
 'use strict';
 
-const rangeOdd = null;
+const rangeOdd = (start, end) => {
+  const res = [];
+  (start % 2 ? start : start++);
+  for (let i = start; i <= end; i += 2) res.push(i);
+  return res;
+};
 
 module.exports = { rangeOdd };
