ce2e67151af1e0c87d8bea9eafc2cef985333b3c
lightballer <lightballer@users.noreply.github.com>
2019-09-19 23:03:33 +0300

    Done 1-9 labs

--- Exercises/9-array.js
+++ Exercises/9-array.js
@@ -1,7 +1,16 @@
 'use strict';
 
-const phonebook = null;
+const phonebook = [
+  { name: 'Peter', phone: '8-800-555-35-35' },
+  { name: 'Artem', phone: '+38-067-123-45-67' },
+];
 
-const findPhoneByName = null;
+const findPhoneByName = name => {
+  for (const item of phonebook) {
+    if (item.name === name) return item.phone;
+  }
+};
+
+console.log(findPhoneByName('Artem'));
 
 module.exports = { phonebook, findPhoneByName };
