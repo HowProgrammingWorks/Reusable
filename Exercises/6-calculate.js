d0d1129a076873ea3a59ffc8cd700f28d66d4e2f
Nikolai Klevanski <nikolai.klevanski@gmail.com>
2019-09-19 18:37:27 +0300

    add tasks 1-6

--- Exercises/6-calculate.js
+++ Exercises/6-calculate.js
@@ -1,11 +1,17 @@
 'use strict';
 
-const square = null;
+const square = a => a * a;
 
-const cube = null;
+const cube = a => a ** 3;
 
-const average = null;
+const average = (a, b) => (a + b) / 2;
 
-const calculate = null;
+const calculate = () => {
+  const arr = [];
+  for (let i = 0; i <= 9; i++) {
+    arr.push(average(square(i), cube(i)));
+  }
+  return arr;
+};
 
 module.exports = { square, cube, average, calculate };
