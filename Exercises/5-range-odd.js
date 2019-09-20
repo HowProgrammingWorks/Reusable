1296c89988c110984030944796d41c5b6a054a17
Kathrin <kathrin.shpigel.90@gmail.com>
2019-09-20 14:29:32 +0300

    Correction of mistakes Reusable

--- Exercises/5-range-odd.js
+++ Exercises/5-range-odd.js
@@ -2,7 +2,7 @@
 
 const rangeOdd = (start, end) => {
   const res = [];
-  (start % 2 ? start : start++);
+  if (start % 2 === 0) start++;
   for (let i = start; i <= end; i += 2) res.push(i);
   return res;
 };
