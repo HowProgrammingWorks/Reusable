d2b891c93e8bd69a1a887f5dade7df1a8ed38e69
Koroliuk <54640416+Koroliuk@users.noreply.github.com>
2019-09-23 18:00:51 +0300

    Update 8-create.js

--- Exercises/8-create.js
+++ Exercises/8-create.js
@@ -1,10 +1,5 @@
 'use strict';
 
-const createUser = function (name, city) {
-  const obj = {};
-  obj.name = name;
-  obj.city = city;
-  return obj;
-};
+const createUser = (name, city) => ({ name, city });
 
 module.exports = { createUser };
