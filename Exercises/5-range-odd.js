a3853157ecb058a615df2746d49c163e4f88193f
Koroliuk <54640416+Koroliuk@users.noreply.github.com>
2019-09-23 18:01:58 +0300

    Update 5-range-odd.js

--- Exercises/5-range-odd.js
+++ Exercises/5-range-odd.js
@@ -3,7 +3,7 @@
 const rangeOdd = (start, end) => {
   const arr = [];
   for (let i = start; i <= end; i++) {
-    if (i % 2 === 1 || i % 2 === -1) {
+    if (i % 2 !== 0) {
       arr.push(i);
     }
   }
