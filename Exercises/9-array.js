5b5fabdd870e2f148faab517cc9286217047ad95
Ovsky Aleksander <masterguyver@gmail.com>
2019-09-24 10:27:28 +0300

    Labs 1-10

--- Exercises/9-array.js
+++ Exercises/9-array.js
@@ -1,7 +1,10 @@
 'use strict';
 
-const phonebook = new Array();
+const phonebook = [{name: 'Timur Garashenko', phone: '+380445554433'}, {name: 'Artem Trifomovisch', phone: '+380636664499'}];
 
-const findPhoneByName = name => {};
+const findPhoneByName = name => {
+  for (const obj of phonebook)
+    if(obj.name === name) return obj.phone;
+};
 
 module.exports = { phonebook, findPhoneByName };
