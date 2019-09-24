1897648fada3a023b7f73accfc1cf9ace76d12e7
Nick Everhard <everhard@fex.net>
2019-09-24 11:40:51 +0300

    Явно указывает проверку на нечетность

--- Exercises/5-range-odd.js
+++ Exercises/5-range-odd.js
@@ -3,7 +3,7 @@
 const rangeOdd = (start, end) => {
   const result = [];
   for (let i = start; i <= end; i++) {
-    if (i % 2) result.push(i);
+    if (i % 2 !== 0) result.push(i);
   }
   return result;
 };
