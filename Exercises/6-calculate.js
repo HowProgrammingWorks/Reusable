6f43ae9ed1d0c272afa13421002e7afdc505e127
Ovsky Aleksander <masterguyver@gmail.com>
2019-09-26 10:53:19 +0300

    Update Exercises/6-calculate.js
    Co-Authored-By: Timur Shemsedinov <timur.shemsedinov@gmail.com>

--- Exercises/6-calculate.js
+++ Exercises/6-calculate.js
@@ -4,7 +4,7 @@ const square = x => x * x;
 
 const cube = x => x ** 3;
 
-const average = (a, b) => ((a+b)*0.5);
+const average = (a, b) => (a + b) / 2;
 
 const calculate = () => {
   const arr = [];
