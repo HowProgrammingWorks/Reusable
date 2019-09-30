d3f807c56cd211b6080ea3af6984bb2dbdff51d8
Roman <hranitel.52@gmail.com>
2019-09-30 22:20:36 +0300

    tasks done

--- Exercises/4-range.js
+++ Exercises/4-range.js
@@ -1,5 +1,12 @@
 'use strict';
 
-const range = null;
+const range = (start, end) => {
+	let array = [];
+
+	for(let i=start; i<=end; ++i)
+		array.push(i);
+
+	return array;
+};
 
 module.exports = { range };
