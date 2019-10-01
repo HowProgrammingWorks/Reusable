fb376154f2252a7275419f7eff1f5abb07059445
Roman Khranivskiy <40178292+GidraFive@users.noreply.github.com>
2019-10-01 17:01:59 +0300

    Update Exercises/9-array.js
    Co-Authored-By: Timur Shemsedinov <timur.shemsedinov@gmail.com>

--- Exercises/9-array.js
+++ Exercises/9-array.js
@@ -7,7 +7,7 @@ const phonebook = [
     { name: 'Marks Aurels',    phone: '+380445786433' }
 ];
 
-const findPhoneByName = (name) => {
+const findPhoneByName = name => {
   for (const p of phonebook) {
     if (p.name === name) {
     return p;
