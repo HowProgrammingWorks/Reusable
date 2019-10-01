92413839c7e1004391ecefb8a807816c42816f3a
Andrey <Andrey@localhost.localdomain>
2019-10-01 18:43:46 +0500

    All exercises completed.

--- Exercises/a-hash.js
+++ Exercises/a-hash.js
@@ -1,7 +1,10 @@
 'use strict';
 
-const phonebook = null;
+const phonebook = {
+  'Marcus Aurelius2': '+380445554433',
+  'Marc Aurelius2': '+70445554433',
+};
 
-const findPhoneByName = null;
+const findPhoneByName = name => phonebook[name];
 
 module.exports = { phonebook, findPhoneByName };
