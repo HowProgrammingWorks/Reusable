1edbb18f98f82276b6650aa8fb1643bf05e6b2f7
vadymshturkhal <vadymshturkhal@gmail.com>
2019-12-03 09:17:29 +0200

    added examples for Python

---
+++ Python/1_identifiers_function_hello.py
@@ -0,0 +1,9 @@
+# Создайте функцию, которая печатает приветствие и имеет один аргумент: name.
+
+
+def function_for_print_hello(name):
+    print('Hello', name)
+
+
+if __name__ == '__main__':
+    function_for_print_hello('Mark')
