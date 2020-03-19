2a89a9422180d0e7dd4e0d22d38084a68d490b03
Nikolaikolya <starcrafters@ya.ru>
2020-03-19 11:59:52 +0700

    Good works is 1 by 9a.

--- Exercises/6-calculate.js
+++ Exercises/6-calculate.js
@@ -1,11 +1,19 @@
 'use strict';
 
-const square = null;
+const square = x => x * x;
 
-const cube = null;
+const cube = x => x ** 3;
 
-const average = null;
+const average = (a, b) => (a + b) / 2;
 
-const calculate = null;
+const calculate = () => {
+  const arr = [];
+  for (let i = 0; i <= 9; i++) {
+    arr.push(average(square(i), cube(i)));
+  }
+  return arr;
+};
+
+console.log(calculate);
 
 module.exports = { square, cube, average, calculate };
--- Exercises/7-objects.js
+++ Exercises/7-objects.js
@@ -1,5 +1,14 @@
 'use strict';
 
-const fn = null;
+const fn = () => {
+  const object = { name: 'Kolyan' };
+  let object2 = { name: 'Kolyan' };
+
+  object2.name = 'Kolymba';
+  object.name = 'Kolymba';
+
+  object2 = {};
+};
+
 
 module.exports = { fn };
