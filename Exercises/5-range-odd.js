0bf013c3920bc1c68db500718b32a452e2029ad1
Vitaliy Korolyk <vitaliykorolyk95@gmail.com>
2020-05-03 18:07:14 +0300

    fix5-range-odd(bad version)#4

--- Exercises/5-range-odd.js
+++ Exercises/5-range-odd.js
@@ -3,13 +3,12 @@
 const rangeOdd = (start, end) => {
   const arr = [];
   for (let i = start; i <= end; i++) {
-    if ((start + i) % 2 !== 0) {
+    if (i % 2 !== 0) {
       arr.push(i);
     }
   }
   return arr;
 };
-
 
 module.exports = { rangeOdd };
 
