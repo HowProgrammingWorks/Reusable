d40438104006d889661c9d05e1c6b4f31bc32680
vadymshturkhal <vadymshturkhal@gmail.com>
2020-05-26 16:47:43 +0300

    Solved lab tasks 1-9 with js.

--- Exercises/5-range-odd.js
+++ Exercises/5-range-odd.js
@@ -1,5 +1,18 @@
 'use strict';
+/* 
+Реализуйте функцию `rangeOdd(start: number, end: number): array` которая
+отдает массив нечетных чисел из диапазона [15, 30] включая крайние числа.
+*/
 
-const rangeOdd = null;
+const rangeOdd = (start, end) => {
+    const numbers = []
+    for (start; start <= end; ++start){
+        if (start % 2 !== 0) {
+            numbers.push(start);
+        }
+
+    }
+    return numbers;
+}
 
 module.exports = { rangeOdd };
--- Exercises/8-create.js
+++ Exercises/8-create.js
@@ -1,5 +1,14 @@
 'use strict';
 
-const createUser = null;
+/*
+8. Реализуйте функцию `createUser` с сигнатурой
+`createUser(name: string, city: string): object`. Пример вызова:
+`createUser('Marcus Aurelius', 'Roma')` функция должна вернуть объект
+`{ name: 'Marcus Aurelius', city: 'Roma' }`
+*/
+
+const createUser = (name, city) => {
+    return {name, city};
+};
 
 module.exports = { createUser };
