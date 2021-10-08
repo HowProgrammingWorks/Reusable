285301feb523148227a765130a92311ad24a0bea
yarman1 <gordienko045@gmail.com>
2021-10-08 23:59:19 +0300

    lab2

--- Exercises/7-objects.js
+++ Exercises/7-objects.js
@@ -1,5 +1,12 @@
 'use strict';
 
-const fn = null;
+const fn = () => {
+  const object1 = { name: 'Yarik' };
+  let object2 = { name: 'Bodya' };
+  object1.name = 'Bodya';
+  object2.name = 'Yarik';
+  //object1 = { name: 'Vanya' };
+  object2 = { name: 'Andriy' };
+};
 
 module.exports = { fn };
--- Exercises/a-hash.js
+++ Exercises/a-hash.js
@@ -1,7 +1,11 @@
 'use strict';
 
-const phonebook = null;
+const phonebook = {
+  'Vanya': '+380447859437',
+  'Kostya': '+380445033776',
+  'Andriy': '+380447483374',
+};
 
-const findPhoneByName = null;
+const findPhoneByName = (name) => phonebook[name];
 
 module.exports = { phonebook, findPhoneByName };
