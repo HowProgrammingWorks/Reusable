5e113401d4798920e3abea050a1778e6f02d135d
Orbis <infectores@gmail.com>
2021-12-29 10:18:59 +0300

    homework

--- Exercises/1-let.js
+++ Exercises/1-let.js
@@ -1,5 +1,5 @@
 'use strict';
 
-let name = undefined;
+let name = 'Dmitrii';
 
 module.exports = { name };
--- Exercises/a-hash.js
+++ Exercises/a-hash.js
@@ -1,7 +1,10 @@
 'use strict';
 
-const phonebook = null;
+const phonebook = {
+  'Marcus Aurelius I': { phone: '+380445554430' },
+  'Marcus Aurelius II': { phone: '+380445554431' },
+};
 
-const findPhoneByName = null;
+const findPhoneByName = (name) => phonebook[name];
 
 module.exports = { phonebook, findPhoneByName };
