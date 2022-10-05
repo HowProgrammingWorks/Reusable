556a573eeca0392f09dd5b9989e49274bd626471
mibal_ua <9mohapx9@gmail.com>
2022-10-05 10:46:59 +0200

    Complete all of exercises

--- Exercises/9-array.js
+++ Exercises/9-array.js
@@ -1,7 +1,15 @@
 'use strict';
 
-const phonebook = null;
+const phonebook = [
+  { name: 'Michael', phone: '+38095' },
+  { name: 'Andrew', phone: '+38096' },
+  { name: 'Alex', phone: '+38097' },
+];
 
-const findPhoneByName = null;
+const findPhoneByName = (name) => {
+  for (const user of phonebook) {
+    if (user.name === name) return user.phone;
+  }
+};
 
 module.exports = { phonebook, findPhoneByName };
