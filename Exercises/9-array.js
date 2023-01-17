bb90af5126c415941c0ce25a271138a00efe382c
Tima Gixe <timagixe@protonmail.com>
2023-01-17 08:32:56 +0200

    Solves 9-array

--- Exercises/9-array.js
+++ Exercises/9-array.js
@@ -1,7 +1,15 @@
 'use strict';
 
-const phonebook = null;
+const phonebook = [
+  { name: 'Tima', phone: '907-200-8803' },
+  { name: 'Gixe', phone: '907-212-4798' },
+  { name: 'Tima G.', phone: '907-221-1144' },
+];
 
-const findPhoneByName = null;
+const findPhoneByName = (name) => {
+  for (const entry of phonebook) {
+    if (entry.name === name) return entry;
+  }
+};
 
 module.exports = { phonebook, findPhoneByName };
