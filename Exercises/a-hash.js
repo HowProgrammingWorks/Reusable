75e6454a38ecba8b9e41d7f332d4b207ff2b73df
Tima Gixe <timagixe@protonmail.com>
2023-01-17 08:33:02 +0200

    Solves a-hash

--- Exercises/a-hash.js
+++ Exercises/a-hash.js
@@ -1,7 +1,11 @@
 'use strict';
 
-const phonebook = null;
+const phonebook = {
+  Tima: '907-200-8803',
+  Gixe: '907-212-4798',
+  'Tima G.': '907-221-1144'
+};
 
-const findPhoneByName = null;
+const findPhoneByName = (name) => phonebook[name];
 
 module.exports = { phonebook, findPhoneByName };
