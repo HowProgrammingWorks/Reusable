1eb6830ef0461a9b7680451bc1e858e1a07fc4fc
Tima Gixe <timagixe@protonmail.com>
2023-01-17 08:32:30 +0200

    Solves 5-range-odd

--- Exercises/5-range-odd.js
+++ Exercises/5-range-odd.js
@@ -1,5 +1,12 @@
 'use strict';
 
-const rangeOdd = null;
+const rangeOdd = (min, max) => {
+  const array = [];
+  const oddMin = min % 2 ? min : min + 1;
+  for (let value = oddMin; value <= max; value += 2) {
+    array.push(value);
+  }
+  return array;
+};
 
 module.exports = { rangeOdd };
