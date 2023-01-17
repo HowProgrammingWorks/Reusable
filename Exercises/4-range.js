fc29851540c6cb3637e230a208e1f5aeb112f087
Tima Gixe <timagixe@protonmail.com>
2023-01-17 08:32:23 +0200

    Solves 4-range

--- Exercises/4-range.js
+++ Exercises/4-range.js
@@ -1,5 +1,11 @@
 'use strict';
 
-const range = null;
+const range = (min, max) => {
+  const array = [];
+  for (let value = min; value <= max; value++) {
+    array.push(value);
+  }
+  return array;
+};
 
 module.exports = { range };
