0bf8d956e73d2ca01c13aeb2f60cb18ad504c7a4
Tima Gixe <timagixe@protonmail.com>
2023-01-17 08:32:44 +0200

    Solves 7-objects

--- Exercises/7-objects.js
+++ Exercises/7-objects.js
@@ -1,5 +1,12 @@
 'use strict';
 
-const fn = null;
+const fn = () => {
+  const male = { name: undefined };
+  let female = null;
+  male.name = 'Tima';
+  female = { name: undefined };
+  female.name = 'Tima';
+  console.log(male !== female);
+};
 
 module.exports = { fn };
